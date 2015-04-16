1ca60777f01408cb255b77e8b8c45e8fec714f12
Быкова Юлия <bikova@pressindex.ru>
2015-04-16 16:17:18 +0500

    seal app

---
+++ app/src/main/java/com/example/julia/seal/Seal.java
@@ -0,0 +1,48 @@
+package com.example.julia.seal;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+
+public class Seal extends ActionBarActivity {
+
+    MediaPlayer mySound;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seal);
+        mySound = MediaPlayer.create(this, R.raw.seal);
+    }
+
+    public void onClickButton(View view) {
+        mySound.start();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_seal, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+}
